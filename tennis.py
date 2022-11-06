e071aacf5a40512da4e250430fb2b7d20f9afaac
Jack Tobin <tobjack330@gmail.com>
2022-11-06 18:01:23 +0000

    initial commit

---
+++ tennis.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   tennis.py
+@Time    :   2022/11/06 07:51:22
+@Author  :   Jack Tobin
+@Version :   1.0
+@Contact :   tobjack330@gmail.com
+"""
+
+
+import selenium
+from bs4 import BeautifulSoup
+import requests as re
+
+
+def main() -> int:
+    print('This is the bot to book tennis reservations')
+    return 0
+
+
+if __name__ == '__main__':
+    main()
